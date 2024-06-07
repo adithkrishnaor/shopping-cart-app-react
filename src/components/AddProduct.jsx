045a8a2e5b47d1804d739513e5af09ea0c23fb8c
adithkrishnaor <adithkrishnaor@gmail.com>
2024-06-07 22:55:35 +0530

    added nav bar

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const AddProduct = () => {
   return (
     <div>
+        <Navbar/>
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -20,7 +22,7 @@ const AddProduct = () => {
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label for="" className="form-label">Product Title</label>
-                        <textarea className="" id="" className="form-control"></textarea>
+                        <textarea name="" id="" className="form-control"></textarea>
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label for="" className="form-label">Price</label>

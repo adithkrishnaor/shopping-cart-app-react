da093969c9c4d7c7d029387b8d9a45407cfa255b
adithkrishnaor <adithkrishnaor@gmail.com>
2024-06-07 22:32:25 +0530

    first commit

---
+++ src/components/AddProduct.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+
+const AddProduct = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-3">
+                    <center>
+                        <h3>Product Registration</h3>
+                    </center>
+                    <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label for="" className="form-label">Product Title</label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label for="" className="form-label">Product Code</label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label for="" className="form-label">Product Title</label>
+                        <textarea className="" id="" className="form-control"></textarea>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label for="" className="form-label">Price</label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label for="" className="form-label">Product Title</label>
+                        <select name="" id="" className="form-control">
+                            <option value="Smart Phones">Smart Phones</option>
+                            <option value="Laptops">Laptops</option>
+                            <option value="Televisions">Televisions</option>
+                            <option value="Earphones">Earphones</option>
+                            <option value="Smart Watches">Smart Watches</option>
+                            <option value="Speakers">Speakers</option>
+                        </select>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label for="" className="form-label">Manufacturing Date</label>
+                        <input type="date" name="" id="" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label for="" className="form-label">Expiry Date</label>
+                        <input type="date" name="" id="" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label for="" className="form-label">Brand</label>
+                        <select name="" id="" className="form-control">
+                            <option value="Sony">Sony</option>
+                            <option value="Lenovo">Lenovo</option>
+                            <option value="HP">HP</option>
+                            <option value="Apple">Apple</option>
+                            <option value="Samsung">Samsung</option>
+                            <option value="Asus">Asus</option>
+                        </select>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label for="" className="form-label">Customer care number</label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label for="" className="form-label">Product Image</label>
+                        <input type="file" name="" id="" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label for="" className="form-label">Distributer Name</label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <button className="btn btn-success">Submit</button>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddProduct
